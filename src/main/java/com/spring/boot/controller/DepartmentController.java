dc54554338c39b71034a643ddeb297890473beb5
rab-tab <sdetcode@gmail.com>
2021-11-12 19:09:17 +0530

    added logger

--- src/main/java/com/spring/boot/controller/DepartmentController.java
+++ src/main/java/com/spring/boot/controller/DepartmentController.java
@@ -3,6 +3,8 @@ package com.spring.boot.controller;
 import com.spring.boot.entity.Department;
 import com.spring.boot.error.DepartmentNotFoundException;
 import com.spring.boot.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,13 +17,18 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER =
+            LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("Inside saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList() {
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
         return departmentService.fetchDepartmentList();
     }
 
