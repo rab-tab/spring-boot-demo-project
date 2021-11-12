50c2f92f9038c8ca45c6f99753ccd1c7a4ee01ae
rab-tab <sdetcode@gmail.com>
2021-11-12 18:37:03 +0530

    hibernate validations

--- src/main/java/com/spring/boot/entity/Department.java
+++ src/main/java/com/spring/boot/entity/Department.java
@@ -5,12 +5,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long departmentId;
+
+    @NotBlank(message = "Please Add Department Name")
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
